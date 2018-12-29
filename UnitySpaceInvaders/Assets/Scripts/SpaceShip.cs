db2530f7218cc7483aa9ab4752a0e5166569be6f
geektechniquestudios <42378648+geektechniquestudios@users.noreply.github.com>
2018-12-29 06:57:30 -0500

    added sprites, scripts, gameboard

---
+++ UnitySpaceInvaders/Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+
+    public float speed = 30;
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
