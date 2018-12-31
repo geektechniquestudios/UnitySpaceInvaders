78ed9cfa8ad0875574b09b406f3f132e6bff6c09
geektechniquestudios <42378648+geektechniquestudios@users.noreply.github.com>
2018-12-30 19:55:15 -0500

    added sound manager code

--- UnitySpaceInvaders/Assets/Scripts/SoundManager.cs
+++ UnitySpaceInvaders/Assets/Scripts/SoundManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager Instance = null;
+    public AudioClip alienBuzz1;
+    public AudioClip alienBuzz2;
+    public AudioClip alienDies;
+    public AudioClip bulletFire;
+    public AudioClip shipExplosion;
+
+    private AudioSource soundEffectAudio;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        } else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        AudioSource theSource = GetComponent<AudioSource>();
+        soundEffectAudio = theSource;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlayOneShot(AudioClip clip)
+    {
+        soundEffectAudio.PlayOneShot(clip);
     }
 }
--- UnitySpaceInvaders/Assets/Scripts/SpaceShip.cs
+++ UnitySpaceInvaders/Assets/Scripts/SpaceShip.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpaceShip : MonoBehaviour
-
+{ 
     public float speed = 30;
-{
-    // Start is called before the first frame update
-    void Start()
+
+    private void FixedUpdate()
     {
-        
+        float horzMove = Input.GetAxisRaw("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horzMove, 0) * speed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Jump"))
+        {
+
+        }
     }
 }
